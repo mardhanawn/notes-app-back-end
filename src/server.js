99a498544507bbccddd10d90036ada9e97775de5
ardhanaawahyu <ardhanaawahyu@bee.id>
2023-05-10 21:20:03 +0700

    create http server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log("Hai semua kita akan membuat RESTful API");
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: "localhost",
+  });
+
+  await server.start();
+  console.log(`Server anda berjalan pada ${server.info.uri}`);
+};
+
+init();
